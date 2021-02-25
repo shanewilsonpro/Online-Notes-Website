f3054b5c6c9e43031209055bce5cb3892910dc08
Shane Wilson <shanewilsonpro@gmail.com>
2021-02-24 20:09:21 -0800

    added functionality for update note

--- mynotes.js
+++ mynotes.js
@@ -7,12 +7,13 @@ $(function() {
         url: "loadnotes.php",
         success: function (data) {
             $('#notes').html(data);
-            clickonNote(); clickonDelete();
+            clickonNote(); 
+            clickonDelete();
             
         },
         error: function() {
             $('#alertContent').text("There was an error with the Ajax Call. Please try again later.");
-                    $("#alert").fadeIn();
+            $("#alert").fadeIn();
         }
     
     });
@@ -37,37 +38,31 @@ $(function() {
             },
             error: function() {
                 $('#alertContent').text("There was an error with the Ajax Call. Please try again later.");
-                    $("#alert").fadeIn();
+                $("#alert").fadeIn();
             }
-        
-        
         });
-    
-    
     });
 
     //type note: : Ajax call to updatenote.php
-    // $("textarea").keyup(function(){
-    //     //ajax call to update the task of id activenote
-    //     $.ajax({
-    //         url: "updatenote.php",
-    //         type: "POST",
-    //         //we need to send the current note content with its id to the php file
-    //         data: {note: $(this).val(), id:activeNote},
-    //         success: function (data) {
-    //             if(data == 'error') {
-    //                 $('#alertContent').text("There was an issue updating the note in the database!");
-    //                 $("#alert").fadeIn();
-    //             }
-    //         },
-    //         error: function() {
-    //             $('#alertContent').text("There was an error with the Ajax Call. Please try again later.");
-    //                     $("#alert").fadeIn();
-    //         }
-
-    //     });
-        
-    // });
+    $("textarea").keyup(function(){
+        //ajax call to update the task of id activenote
+        $.ajax({
+            url: "updatenote.php",
+            type: "POST",
+            //we need to send the current note content with its id to the php file
+            data: {note: $(this).val(), id:activeNote},
+            success: function (data) {
+                if(data == 'error') {
+                    $('#alertContent').text("There was an issue updating the note in the database!");
+                    $("#alert").fadeIn();
+                }
+            },
+            error: function() {
+                $('#alertContent').text("There was an error with the Ajax Call. Please try again later.");
+                $("#alert").fadeIn();
+            }
+        });
+    });
     
     //click on all notes button
     $("#allNotes").click(function() {
@@ -76,11 +71,12 @@ $(function() {
             success: function (data) {
                 $('#notes').html(data);
                 showHide(["#addNote", "#edit", "#notes"], ["#allNotes", "#notePad"]);
-                clickonNote(); clickonDelete();
+                clickonNote(); 
+                clickonDelete();
             },
             error: function() {
                 $('#alertContent').text("There was an error with the Ajax Call. Please try again later.");
-                        $("#alert").fadeIn();
+                $("#alert").fadeIn();
             }
 
         });
@@ -110,20 +106,20 @@ $(function() {
     // });
     
     //click on a note function
-    // function clickonNote() {              
-    //         $(".noteheader").click(function() {
-    //         if(!editMode) {
-    //             //update activeNote variable to id of note
-    //             activeNote = $(this).attr("id");
+    function clickonNote() {              
+            $(".noteheader").click(function() {
+            if(!editMode) {
+                //update activeNote variable to id of note
+                activeNote = $(this).attr("id");
 
-    //             //fill text area
-    //             $("textarea").val($(this).find('.text').text());
-    //             //show hide elements
-    //             showHide(["#notePad", "#allNotes"], ["#notes", "#addNote", "#edit", "#done"]);
-    //             $("textarea").focus();
-    //         }
-    //     });
-    // }
+                //fill text area
+                $("textarea").val($(this).find('.text').text());
+                //show hide elements
+                showHide(["#notePad", "#allNotes"], ["#notes", "#addNote", "#edit", "#done"]);
+                $("textarea").focus();
+            }
+        });
+    }
     
     //click on delete
     // function clickonDelete() {
